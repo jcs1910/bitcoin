48d289427ee537aa2d9a85f8207ed51216992496
dokdo <jcs191072@gmail.com>
2020-11-06 19:57:17 +0900

    2_test.js 파일을 올림

---
+++ basic_blockchain/2_test.js
@@ -0,0 +1,6 @@
+const Blockchain = require('./2.genesisBlock');
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock('sgdsgdsgwgwt324', 'gjkvbnkeagl35435klllk', 1000);
+
+console.log(bitcoin);
